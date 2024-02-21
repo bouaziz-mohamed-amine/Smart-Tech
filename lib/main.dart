d9388c8bbce48180eb3890d8c45fce47b671d66b
amine <amine.m.a1998@gmail.com>
2024-02-21 20:13:08 +0100

    create MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:smattech/HomePage.dart';
 
 void main() {
-
-
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),));
+  runApp(MyApp());
 }
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),);
+  }
+}
+
+
+
+
