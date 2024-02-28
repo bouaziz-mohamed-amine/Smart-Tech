4b9a5afa11071924f8c2011e5759432924ba9bb4
amine <amine.m.a1998@gmail.com>
2024-02-28 21:15:57 +0100

    use ListView SingleScrollView

--- lib/HistoryPage.dart
+++ lib/HistoryPage.dart
@@ -11,7 +11,311 @@ class _HistoryState extends State<History> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("History"),
+        centerTitle: true,
+        backgroundColor: Colors.green.shade700,
+        leading: IconButton(
+          icon: Icon(Icons.keyboard_arrow_left,size: 30,),
+          onPressed: ()=>{
+          Navigator.pop(context)
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+              height: MediaQuery.of(context).size.height /3,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              )
+          ),
+          Container(
+              height: MediaQuery.of(context).size.height /3,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                children: [
+                  Container(
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 40,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                ],
+              )
+          ),
+          Container(
+              height: MediaQuery.of(context).size.height /5,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ],
+              )
+          ),
+        ],
+      )
     );
   }
 }
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -12,11 +12,16 @@ class _HomePageState extends State<HomePage> {
 
 
   var count;
+  late bool notifications;
+  late bool lights;
+
 
   @override
   void initState() {
     // TODO: implement initState
     count=0;
+    notifications=true;
+    lights=true;
   }
 
   @override
@@ -34,17 +39,17 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Colors.green.shade700,
         actions: [
-          Icon(Icons.notifications_off_outlined),
-          IconButton(
-              onPressed: ()=>{
+          notifications==true ? IconButton(
+            onPressed: ()=>{
               Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => History()
-              ),
+                context,
+                MaterialPageRoute(
+                    builder: (context) => History()
+                ),
               )
-              },
-              icon: Icon(Icons.notifications_active),)
+            },
+            icon: Icon(Icons.notifications_active),) : Icon(Icons.notifications_off_outlined)
+
         ],
       ),
       body: Container(
@@ -53,52 +58,64 @@ class _HomePageState extends State<HomePage> {
         color: Colors.grey.shade100,
         child: Column(
           children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.width /30,
+            ),
             Expanded(
                 child: Container(
                   child: Row(
                     children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width /30 ,
+                      ),
                       Expanded(
                         child: Container(
-                          color: Colors.blue,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width /20),
+                          ),
                           child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: [
                               Container(
-                                width: MediaQuery.of(context).size.width *0.4,
-                                height: MediaQuery.of(context).size.width *0.4,
+                                width: MediaQuery.of(context).size.width *0.38,
+                                height: MediaQuery.of(context).size.width *0.38,
                                 decoration: BoxDecoration(
-                                  color: Colors.red,
+                                  color: lights? Colors.green.shade700 : Colors.grey.shade100,
                                   shape: BoxShape.circle,
                                 ),
                                 child: IconButton(
                                   onPressed: ()=>{
                                     setState(() {
-                                      count++;
+                                      lights=!lights;
                                     })
                                   },
-                                  icon: Icon(Icons.ac_unit,color: Colors.white),
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(
-                                  top: MediaQuery.of(context).size.height*0.01,
-                                  bottom:MediaQuery.of(context).size.height*0.01,
-                                ),
-                                color: Colors.greenAccent,
-                                child: Text("sample container",
-                                  style: TextStyle(
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.w600
-
+                                  icon: Icon(
+                                     Icons.lightbulb_outlined,
+                                    color: lights? Colors.white:Colors.grey,
+                                    size: 28,
                                   ),
                                 ),
                               ),
+                               lights? Text("All Lights On",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600
+
+                                ),
+                              ): Text("All Lights Off",
+                                 style: TextStyle(
+                                     fontSize: 16,
+                                     fontWeight: FontWeight.w600
+
+                                 ),
+                               )
                             ],
                           ),
                         ),
                       ),
-                      Container(
+                      SizedBox(
                         width: MediaQuery.of(context).size.width /30 ,
                       ),
                       Expanded(
@@ -129,7 +146,7 @@ class _HomePageState extends State<HomePage> {
                                       bottom:MediaQuery.of(context).size.height*0.01,
                                     ),
                                     color: Colors.greenAccent,
-                                    child: Text("sample container",
+                                    child: Text("Electricity  On/Off",
                                       style: TextStyle(
                                           fontSize: 17,
                                           fontWeight: FontWeight.w600
@@ -142,9 +159,15 @@ class _HomePageState extends State<HomePage> {
                             ),
                           )
                       ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width /30 ,
+                      ),
                     ],
                   ),
                 )
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.width /30,
             ),
             Expanded(
                 child: Container(
@@ -179,7 +202,7 @@ class _HomePageState extends State<HomePage> {
                                 bottom:MediaQuery.of(context).size.height*0.01,
                             ),
                             color: Colors.greenAccent,
-                            child: Text("sample container",
+                            child: Text("Weather "+37.toString(),
                             style: TextStyle(
                               fontSize: 17,
                               fontWeight: FontWeight.w600
@@ -222,7 +245,7 @@ class _HomePageState extends State<HomePage> {
                             bottom:MediaQuery.of(context).size.height*0.01,
                           ),
                           color: Colors.greenAccent,
-                          child: Text("sample container",
+                          child: Text("window On/Off",
                             style: TextStyle(
                                 fontSize: 17,
                                 fontWeight: FontWeight.w600
@@ -238,6 +261,9 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             )
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.width /30,
             ),
             Expanded(
                 child: Container(
@@ -270,7 +296,7 @@ class _HomePageState extends State<HomePage> {
                                     bottom: 10
                                   ),
                                   color: Colors.blue,
-                                  child: Text("sample container"),
+                                  child: Text("Everything off"),
                                 ),
                               ],
                             ),
@@ -280,34 +306,44 @@ class _HomePageState extends State<HomePage> {
                         width: MediaQuery.of(context).size.width /30 ,
                       ),
                       Expanded(
-                          child: InkWell(
-                            onTap: ()=>{
-                              setState(() {
-                                count--;
-                              })
-                            },
-                            child: Container(
-                              color: Colors.blue,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    width: MediaQuery.of(context).size.width*0.4,
-                                    height: MediaQuery.of(context).size.width*0.4,
-                                    decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Colors.red
-                                    ),
-                                    child: Icon(Icons.access_alarms,color: Colors.white,),
+                          child: Container(
+                            color: Colors.red,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.blue,
                                   ),
-                                  Text(count.toString()),
-                                ],),),
+                                  width: MediaQuery.of(context).size.width *0.4,
+                                  height: MediaQuery.of(context).size.width *0.4,
+                                  child: IconButton(
+                                      onPressed: ()=>{
+                                        setState(() {
+                                          count++;
+                                        })
+                                      },
+                                      icon: Icon(Icons.add_a_photo_rounded,color: Colors.white,)),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.only(
+                                      top: 10,
+                                      bottom: 10
+                                  ),
+                                  color: Colors.blue,
+                                  child: Text("TV Time On/Off"),
+                                ),
+                              ],
+                            ),
                           )
                       ),
                     ],
                   ),
-                ))
+                )),
+            SizedBox(
+              height: MediaQuery.of(context).size.width /30,
+            ),
           ],
         ),
       ),
